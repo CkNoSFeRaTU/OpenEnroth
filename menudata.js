49c2112d979fe1db12cf773e4ef3d593522cf512
CkNoSFeRaTU <master.nosferatu@gmail.com>
2022-12-25 22:22:40 +0000

    Deploying to gh-pages from @ CkNoSFeRaTU/world-of-might-and-magic@53eee8049c9b141fd723feb902b1152f6e9a293f 🚀

--- menudata.js
+++ menudata.js
@@ -272,11 +272,9 @@ var menudata={children:[
 {text:"h",url:"globals_defs_h.html#index_h"},
 {text:"l",url:"globals_defs_l.html#index_l"},
 {text:"m",url:"globals_defs_m.html#index_m"},
-{text:"n",url:"globals_defs_n.html#index_n"},
 {text:"p",url:"globals_defs_p.html#index_p"},
 {text:"r",url:"globals_defs_r.html#index_r"},
 {text:"s",url:"globals_defs_s.html#index_s"},
 {text:"t",url:"globals_defs_t.html#index_t"},
 {text:"u",url:"globals_defs_u.html#index_u"},
-{text:"v",url:"globals_defs_v.html#index_v"},
 {text:"w",url:"globals_defs_w.html#index_w"}]}]}]}]}
